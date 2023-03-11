e3b06f118a46e0520ce8c58d5f3584f289657271
Alexey Artyush <artush.alexey@gmail.com>
2023-03-12 00:15:04 +0300

    Add contact form

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,25 @@
 new Swiper('.swiper', {
+    spaceBetween: 5,
+    slidesPerView: 5,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+    },
 
-}
-)
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
